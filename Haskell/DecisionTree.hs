4b442c4b8e359bda2f88ce9904ededd2f1db4b88
Luke Abruzese <24labruzese@xbhs.net>
2024-04-14 13:46:17 -0400

    refactored structure and defined module to replace old dt

--- Haskell/DecisionTree.hs
+++ Haskell/DecisionTree.hs
@@ -1,44 +1,75 @@
--- import CSVLoader
--- import GHC.Float (log1pDouble)
--- import GHC.Generics (D)
+module DecisionTree where
 
--- --Reduction in entropy due to testing a category(might not be needed)
--- --informationGain :: Double -> Int -> CategoryTable -> Double
--- --informationGain tableEntropy category table = tableEntropy - entropyRemainingAfterTest category table
+import CSVLoader
+import Data.List ( maximumBy, elemIndex, nub, transpose )
+import Data.Foldable (maximumBy)
+import Data.Function (on)
+import Data.Ord (comparing)
 
--- --The entropy after testing a category
--- entropyRemainingAfterTest :: Int -> CategoryTable -> Double
--- entropyRemainingAfterTest colNum ct = sum $ map (`binEntropy` n) bs
---     where
---         bs = bins colNum ct
---         n = numEntries ct
+data NodeData = Question String | Answer Bool
 
--- binEntropy :: Bin -> Int -> Double
--- binEntropy bin numEntries = entropyOfBernoulliVariable (pCount/nBin) * nBin / nTotal
---     where
---         pCount = fromIntegral $ successCount bin
---         nBin = fromIntegral $ count bin
---         nTotal = fromIntegral numEntries
+data Edge = Edge { label :: String, destination :: Node }
+data Node = Node { nodeData :: NodeData, edges :: [Edge] }
 
--- occurences :: String -> [String] -> Int
--- occurences match = length . filter (== match)
+--DECISION TREE MAIN--
 
--- --bestEntropy :: CategoryTable -> [String]
--- --bestEntropy catTable = max $ mapWithIndex (\i -> entropyRemainingAfterTest i catTable) catTable
+decisionTree :: [CategoryTable] -> Node
+decisionTree (currCT : prevCTS)
+    | null $ tail $ successColumn currCT = plurityValue (head prevCTS)
+    | and $ tail $ successColumn currCT = Node (Answer $ successColumn currCT !! 1) []
+    | null $ tail currCT = plurityValue currCT
+    | otherwise = Node (Question $ head bestCategory) (zipWith edgeCreator (featuresIn bestCategory) (subsets bestCategory currCT))
+    where
+        plurityValue category = Node (Answer $ mostFrequentItem $ successColumn category) []
+        bestCategory = maximumBy (comparing $ importance currCT) currCT
+        bestCategoryID = findIndexInList bestCategory currCT
+        edgeCreator label newCTS = Edge label $ decisionTree (newCTS : prevCTS)
 
--- totalEntropy :: CategoryTable -> Double
--- totalEntropy ct = entropyOfBernoulliVariable $ fromIntegral trueCount / fromIntegral totalCount
---     where
---         successCol = successColumn ct
---         trueCount = length $ filter id successCol
---         totalCount = length successCol
+subsets :: Category -> CategoryTable -> [CategoryTable]
+subsets cat ct = map (\x -> subset ct x (findIndexInList cat ct)) (featuresIn cat)
 
--- --The entropy of a Boolean random variable that is true with probability p
--- entropyOfBernoulliVariable :: Double -> Double
--- entropyOfBernoulliVariable 0 = 0
--- entropyOfBernoulliVariable 1 = 0
--- entropyOfBernoulliVariable p = -(p * log2 p + q * log2 q)
---     where
---         q = 1-p
---         log2 = logBase 2
+subset :: CategoryTable -> String -> Int -> CategoryTable
+subset ct feature categoryID = transpose $ filter (\example -> example !! categoryID /= feature) exampleTable
+    where exampleTable = transpose ct
 
+featuresIn :: Category -> [String]
+featuresIn cat = nub $ tail cat
+
+--ENTROPY--
+
+importance :: CategoryTable -> Category -> Double
+importance ct cat = entropyOfBool pGoal - remainingEntropy ct cat
+    where 
+        pGoal = p / (p+n)
+        p = fromIntegral $ posExamples ct
+        n = fromIntegral $ negExamples ct
+
+remainingEntropy :: CategoryTable -> Category -> Double
+remainingEntropy ct cat = sum $ map (\s -> proportionRemaining s * entropyOfBool (propSuccess s)) (subsets cat ct)
+    where
+        proportionRemaining sub = examples sub / examples ct
+        propSuccess sub = p sub / examples ct
+        p ctable = fromIntegral $ posExamples ctable
+        examples ctable = fromIntegral $ numEntries ctable
+
+entropyOfBool :: Double -> Double
+entropyOfBool 0 = 0
+entropyOfBool 1 = 1
+entropyOfBool probability = -( sum $ map entropyOfVar [probability, 1-probability] )
+    where
+        entropyOfVar x = x * log2 x
+        log2 = logBase 2
+
+
+--HELPERS--
+
+findIndexInList :: Eq a => a -> [a] -> Int
+findIndexInList x xs =
+  case elemIndex x xs of
+    Just idx -> idx
+    Nothing  -> -1
+
+mostFrequentItem :: (Eq a) => [a] -> a
+mostFrequentItem (x:xs) = fst $ maximumBy (compare `on` snd) $ map (\y -> (y, count y (x:xs))) (x:xs)
+    where
+        count x = length . filter (== x)
