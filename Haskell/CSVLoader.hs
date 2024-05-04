a8511492b74cb54f8a153b5e33066e240efab83d
Luke Abruzese <24labruzese@xbhs.net>
2024-05-04 17:28:49 -0400

    caged isacceptable to be a lamba

--- Haskell/CSVLoader.hs
+++ Haskell/CSVLoader.hs
@@ -11,41 +11,51 @@ trim,
 trim2,
 headers,
 entries,
-loadTrainingSet
+smallmain
 ) where
 
 import System.IO
 import Data.Map.Strict (insertWith, Map, singleton, toList)
 import qualified Data.Map.Strict as Map
+import Text.Read
 
 import Data.List
-    ( isInfixOf, transpose, nub, foldl', minimumBy, tails, sortBy )
+    ( isInfixOf,
+      transpose,
+      nub,
+      foldl',
+      minimumBy,
+      tails,
+      sortBy,
+      find )
 import Data.Bool
 import Distribution.Compat.Prelude (readMaybe)
 import Data.Ord (comparing)
 import Debug.Trace (trace)
-import Haskell.Helpers
-import qualified Data.Map.Strict as Map
+import Helpers ( count, mapIndexes, removeFirst, findIndexInList, findFirstMatchingIndex )
 import Data.Sequence (mapWithIndex)
 
---An array of categories in the format CategoryTable[category][data point]
-    --The first column consists of unique category names
-    --The first row consists of either "True" or "False" except for the first itme which is the category name
 type StringCategoryTable = [StringCategory]
 type StringCategory = [String]
 
-data NewCategoryTable = NewCategoryTable { results :: BoolCategory, predictors :: [BoolCategory] }
+data NewCategoryTable = NewCategoryTable { results :: BoolCategory, predictors :: [BoolCategory] } deriving (Show)
 
-data BoolCategory = BoolCategory { question :: String, dataPoints :: CategoryData }
-type CategoryData = [Bool] -- make missing part of the features
+data BoolCategory = BoolCategory { question :: String, dataPoints :: CategoryData, isAcceptable :: String -> Bool } deriving (Show)
+type CategoryData = [Bool]
 
-data Entry = Entry { result :: Bool, askQuestion :: Map.Map String Bool}
+data Entry = Entry { result :: Bool, askQuestion :: Map.Map String Bool} deriving (Show)
 
 missing :: String
 missing = "N/A"
 
+successColumnHeader :: String
+successColumnHeader = "CS Req Grade"
+
+minimumSuccess :: String
+minimumSuccess = "B"
+
 numCategories :: NewCategoryTable -> Int
-numCategories x = length predictors
+numCategories x = length $ predictors x
 
 numEntries :: NewCategoryTable -> Int
 numEntries = length . entries
@@ -55,7 +65,10 @@ loadCategoryTable :: FilePath -> IO NewCategoryTable
 loadCategoryTable path = do
     content <- readFile path
     return $ joinToTable . concatMap splitCategory . formatMissing . transpose $ map (splitOn ',') (lines content)
-        where joinToTable cats = NewCategoryTable (head cats) (tail cats)
+        where
+            joinToTable cats = NewCategoryTable  {results = cats !! indexOfSuccessColumn cats, predictors = predictorsColumns cats}
+            indexOfSuccessColumn = findFirstMatchingIndex (\x -> question x `isInfixOf` successColumnHeader)
+            predictorsColumns cats = take (indexOfSuccessColumn cats) cats ++ drop (indexOfSuccessColumn cats +1) cats
 
 splitOn :: Char -> String -> [String]
 splitOn _ [] = []
@@ -67,6 +80,7 @@ splitOn delim xs = case break (== delim) xs of
 --      ~Overall Filters~
 missingFilter :: String -> String
 missingFilter "MISSING" = "N/A"
+missingFilter "" = "N/A"
 missingFilter str = str
 
 --Applies the missing filter
@@ -76,23 +90,33 @@ formatMissing = map (map missingFilter)
 splitCategory :: StringCategory -> [BoolCategory]
 splitCategory category = map buildCategoryAtSplitPoint $ getPossibleSplitPoints (head category) (tail category)
     where
-        buildCategoryAtSplitPoint acceptableFeatures = BoolCategory (newHeader acceptableFeatures) $ map (`elem` acceptableFeatures) (tail category)
-        newHeader acceptableFeatures
-            | missing `elem` acceptableFeatures = head category ++ show acceptableFeatures
+        buildCategoryAtSplitPoint isAcceptableFun = BoolCategory
+            (head category)
+            (map isAcceptableFun (tail category))
+            isAcceptableFun
 
-getPossibleSplitPoints :: String -> StringCategory -> [StringCategory]
+getPossibleSplitPoints :: String -> StringCategory -> [String -> Bool]
 getPossibleSplitPoints name category
-    | "UNMATCHABLE EXAMPLE" `isInfixOf` name = uniqueFeaturesSortedWith gradeValueOf $ filter (/= missing) category
-    --ADD OTHER SORTABLE STUFF HERE
-    | otherwise = combos $ nub category
+    | successColumnHeader `isInfixOf` name = [resultColumnSplit]
+    | "Grade" `isInfixOf` name = uniqueFeaturesSortedWith gradeComparator $ filter (/= missing) category
+    | "Birth" `isInfixOf` name = uniqueFeaturesSortedWith birthMonthComparator $ filter (/= missing) category
+    | "Level" `isInfixOf` name = uniqueFeaturesSortedWith classLevelComparator $ filter (/= missing) category
+    | "Tarties" `isInfixOf` name = uniqueFeaturesSortedWith classLevelComparator $ filter (/= missing) category
+    | "Siblings" `isInfixOf` name = uniqueFeaturesSortedWith siblingComparator $ filter (/= missing) category
+    | otherwise = map (flip elem) (combos $ nub category)
     where
-        uniqueFeaturesSortedWith func features = if missing `elem` features
-            then dividedSortedList func features ++ map (++[missing]) (dividedSortedList func features)
-            else dividedSortedList func features
+        resultColumnSplit = (<= gradeComparator minimumSuccess) . gradeComparator
+
+        uniqueFeaturesSortedWith = if missing `elem` category then dividedSortedListWithMissing else dividedSortedList
 
         dividedSortedList func features = split $ nub $ sortBy (comparing func) features
+        dividedSortedListWithMissing func features = dividedSortedList func features ++ splitWithMissing (nub $ sortBy (comparing func) features)
 
-        split xs = map (`take` xs) [1..length xs - 1]
+        split :: [String] -> [String -> Bool]
+        split = map (<=)
+
+        splitWithMissing :: [String] -> [String -> Bool]
+        splitWithMissing = map (\ x y -> y <= x || y == missing)
 
 combos :: [a] -> [[a]]
 combos list = concatMap specialPick [1..ceiling $ (fromIntegral (length list) - 0.4) / 2.0]
@@ -107,18 +131,13 @@ pickFrom _ 0  = [[]]
 pickFrom [] _ = []
 pickFrom (x:xs) n  = map (x:) (pickFrom xs (n-1)) ++ pickFrom xs n
 
-
-gradeValueOf :: String -> Double
-gradeValueOf x = fromIntegral $ length x
---      ~Specific Filters~
-
---double value for a grade for comparision
 gradeComparator :: String -> Double
 gradeComparator x = charToDouble (head x) + plusMinusAnalysis x
     where
         plusMinusAnalysis x
-            | "+" `isInfixOf` x = -0.5
-            | "-" `isInfixOf` x = 0.5
+            | "+" `isInfixOf` x = -0.33
+            | "-" `isInfixOf` x = 0.33
+            | otherwise = 0
         charToDouble x
             | x == 'A' = 1.0
             | x == 'B' = 2.0
@@ -126,6 +145,28 @@ gradeComparator x = charToDouble (head x) + plusMinusAnalysis x
             | x == 'D' = 4.0
             | x == 'F' = 5.0
             | otherwise = 10.0
+
+birthMonthComparator :: String -> Double
+birthMonthComparator string =
+  let month = read string :: Int
+  in fromIntegral ((month + 3) `mod` 12)
+
+classLevelComparator :: String -> Double
+classLevelComparator "AP" = 1
+classLevelComparator "Honors" = 2
+classLevelComparator "Advanced" = 3
+classLevelComparator "Adv" = 3
+classLevelComparator "Accelerated" = 4
+classLevelComparator "Acclerated" = 4
+classLevelComparator "Standard" = 5
+classLevelComparator "CP" = 6
+classLevelComparator x = error $ "unrecognized class level " ++ show x
+
+abstComparator :: String -> Double
+abstComparator = read
+
+siblingComparator :: String -> Double
+siblingComparator = read
 
 posExamples :: NewCategoryTable -> Int
 posExamples ct = count True $ dataPoints $ results ct
@@ -137,19 +178,30 @@ headers :: StringCategoryTable -> [String]
 headers = map head
 
 entries :: NewCategoryTable -> [Entry]
-entries ct = mapIndexes (\(entryId, entry) -> buildEntry (dataPoints (results ct) !! entryId) entry) $ transpose $  
+entries ct = mapIndexes (\(entryId, entry) -> buildEntry (dataPoints (results ct) !! entryId) entry) $ transpose $
     map (\category -> map (question category,) (dataPoints category)) (predictors ct)
     where
         buildEntry result qAndA = Entry result $ Map.fromList qAndA
 
 
-trim :: Int -> StringCategoryTable -> StringCategoryTable
-trim sp x = transpose $ map (transpose x !!) [0..sp]
-trim2 :: Int -> StringCategoryTable -> StringCategoryTable
-trim2 sp x = transpose $ map (transpose x !!) (0 : [sp+1..numEntries x])
+trim :: Int -> NewCategoryTable -> NewCategoryTable
+trim sp ct = NewCategoryTable {
+    results = editDataPoints (results ct) (take sp $ dataPoints $ results ct),
+    predictors = map (\x -> editDataPoints x $ take sp (dataPoints x)) (predictors ct)
+    }
 
+trim2 :: Int -> NewCategoryTable -> NewCategoryTable
+trim2 sp ct = NewCategoryTable {
+    results = editDataPoints (results ct) (drop sp $ dataPoints $ results ct),
+    predictors = map (\x -> editDataPoints x $ drop sp (dataPoints x)) (predictors ct)
+    }
+
+trimCategories :: StringCategoryTable -> StringCategoryTable
+trimCategories = take 5
 
 main = loadCategoryTable "49dtd.csv"
 
-loadTrainingSet = loadCategoryTable "wikiTest.csv"
+smallmain = loadCategoryTable "wikiTest.csv"
 
+-- editDataPoints cat newdps = BoolCategory { question = question cat, dataPoints = newdps, acceptableFeatures = acceptableFeatures cat }
+-- editQuestion cat newQ = BoolCategory { question = newQ, dataPoints = dataPoints cat, acceptableFeatures = acceptableFeatures cat }
--- Haskell/Helpers.hs
+++ Haskell/Helpers.hs
@@ -1,6 +1,25 @@
-module Haskell.Helpers where
+module Helpers where
+import Data.List (elemIndex)
 count :: Eq a => a -> [a] -> Int
 count x = length . filter (== x)
 
 mapIndexes :: ((Int, b) -> c) -> [b] -> [c]
 mapIndexes fun a = zipWith (curry fun) [0..length a - 1] a
+
+removeFirst :: Eq a => a -> [a] -> [a]
+removeFirst _ [] = []
+removeFirst x (y:ys)
+  | x == y    = ys
+  | otherwise = y : removeFirst x ys
+
+findIndexInList :: Eq a => a -> [a] -> Int
+findIndexInList x xs =
+  case elemIndex x xs of
+    Just idx -> idx
+    Nothing  -> error "unable to find element in list"
+
+findFirstMatchingIndex :: (a -> Bool) -> [a] -> Int
+findFirstMatchingIndex _ [] = error "unable to find element in list"
+findFirstMatchingIndex p (x:xs)
+    | p x = 0
+    | otherwise = 1 + findFirstMatchingIndex p xs
